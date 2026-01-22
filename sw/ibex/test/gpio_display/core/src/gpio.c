59c207bae05d242f2bd4078085d472c2ac1b9968
Rock Moose <rock@rock-moose-pc>
2026-01-22 13:30:00 +0200

    removed done gpio_display

--- sw/ibex/test/gpio_display/core/src/gpio.c
+++ sw/ibex/test/gpio_display/core/src/gpio.c
@@ -11,17 +11,9 @@ volatile uint16_t stepper = 255;    // Checks half-word instructions
 
 volatile uint32_t  gpio_int_status = 0; // Holds interrupt status
 
-uint8_t numbers [10] = {0b00111111, // 0
-                        0b00000110, // 1
-                        0b01011011, // 2
-                        0b01001111, // 3
-                        0b01100110, // 4
-                        0b01101101, // 5
-                        0b01111101, // 6
-                        0b00000111, // 7
-                        0b01111111, // 8
-                        0b01101111, // 9
-};
+void display_number(uint32_t value);
+
+void display_number_timed(uint32_t value, uint32_t duration_us);
 
 int main() {
     //####################################################
@@ -32,18 +24,13 @@ int main() {
     gpio_regs = (uint32_t*) GPIO_ADDR; // First GPIO reg - output register
 
     // All segments test one by one
-    *(gpio_regs + 2) = 0xFF;
-    for (int i = 0; i < 7; i++) {
-        *(gpio_regs + 1) = (0x01 << i);
-    }
+    
 
     // All segments on
-    *(gpio_regs + 1) = 0xFF;
+    
 
     // Counts from 0 to 9
-    for (int i = 0; i < 10; i++) {
-        *(gpio_regs + 1) = numbers[i];
-    }
+    
 }
 
 void gpio_handler(void){
