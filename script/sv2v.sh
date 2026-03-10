aa4c2937ce4ba93ec7c5f4cb8ff410e3769eee1c
dovydasliutkus <dovydasliutkus02@gmail.com>
2026-03-10 20:52:37 +0100

    feat(rtl): generated verilog, fixed last_read problem by hand

---
+++ script/sv2v.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+readonly PROJECT_ROOT="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/.."
+
+cd "$PROJECT_ROOT/sv2v" || exit
+
+
+export DESIGN="ibex_simple_system"
+file_list="file_list.f"
+
+mkdir -p "generated"
+mkdir -p "log"
+
+# reads file list
+files=""
+while read file; do
+  module=$(basename -s .sv "$file")
+  files+="$file "
+done < $file_list
+
+# echo "$files"
+
+# converts all files from file list from SystemVerilog to Verilog and combines them too one file
+    sv2v \
+    --define=SYNTHESIS \
+    --define=YOSYS \
+    -I../rtl/primitives/ \
+    -I../rtl/include/ \
+    $files \
+    > "./generated/$DESIGN.v" \
+    2> "./log/$DESIGN.log"
