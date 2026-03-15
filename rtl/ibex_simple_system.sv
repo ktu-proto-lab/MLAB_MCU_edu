a9044e9cc32617482002f2b7533f0fe6326a4f79
Rock Moose <rock@rock-moose-PC8>
2026-03-15 21:27:56 +0200

    removed bootloader, uart, gpio and even timer (still no luck with tinytapeout. GDS failed

--- rtl/ibex_simple_system.sv
+++ rtl/ibex_simple_system.sv
@@ -17,10 +17,10 @@ module ibex_simple_system #(
   // output  logic      sda_pad_o,
   // output  logic      sda_padoen_o,
   
-  input   logic [`GPIO_IOS-1:0] ext_pad_i,
-
-  output  logic [`GPIO_IOS-1:0] gpio_o,
-  output  logic [`GPIO_IOS-1:0] gpio_oe,
+  // input   logic [`GPIO_IOS-1:0] ext_pad_i,
+  //
+  // output  logic [`GPIO_IOS-1:0] gpio_o,
+  // output  logic [`GPIO_IOS-1:0] gpio_oe,
 
   output logic o_qspi_sck,
   output logic o_qspi_cs_n,
@@ -42,7 +42,7 @@ module ibex_simple_system #(
   
   // // GPIO signals
   logic gpio_int; // Interrupt signal to Core
-  logic [`GPIO_IOS-1:0] gpio_aux; // Auxiliary input 
+  // logic [`GPIO_IOS-1:0] gpio_aux; // Auxiliary input
   
   // I2C signals
   logic      i2c_int; 
@@ -53,21 +53,21 @@ module ibex_simple_system #(
 
   // UART signals
   
-  `ifdef UART_NO_FIFO
-  `else
-  logic	     uart_ncts_i;
-  logic	     uart_nrts_o;
-  
-  logic	     uart_rxfifo_int_o;
-  logic      uart_txfifo_int_o;
-  `endif
-  
-  logic	     uart_rx_int_o;
-  logic	     uart_tx_int_o;
+  // `ifdef UART_NO_FIFO
+  // `else
+  // logic	     uart_ncts_i;
+  // logic	     uart_nrts_o;
+  //
+  // logic	     uart_rxfifo_int_o;
+  // logic      uart_txfifo_int_o;
+  // `endif
+  //
+  // logic	     uart_rx_int_o;
+  // logic	     uart_tx_int_o;
   
 
-  logic      uart_rx_i;
-  logic      uart_tx_o;
+  // logic      uart_rx_i;
+  // logic      uart_tx_o;
 
   // PIT signals
   logic      pit_irq;   // Interrupt Timer Request signal to Core
@@ -76,8 +76,8 @@ module ibex_simple_system #(
   // Local Parameters
   //==================================================
 
-  localparam int NUM_MASTERS        = 3;
-  localparam int NUM_SLAVES         = 4;
+  localparam int NUM_MASTERS        = 2;
+  localparam int NUM_SLAVES         = 1;
   localparam int PIT_SLAVE_PORT_NUM = 4;
 
   //==================================================
@@ -126,12 +126,13 @@ module ibex_simple_system #(
   wb_if wbm[NUM_MASTERS] (.rst(rst_sync_n), .clk(clk_sys));  // Masters
   wb_if wbs[NUM_SLAVES] (.rst(rst_sync_n), .clk(clk_sys));  // Slaves
 
+  assign rst_core_n = rst_sync_n;
   // Bootloader as wishbone master with top priority
-  bootloader_top u_bootloader(
-        .wb(wbm[0]),
-        .clk(clk_sys),
-        .rst_core_n(rst_core_n) // rst for Ibex only
-  );
+  // bootloader_top u_bootloader(
+  //       .wb(wbm[0]),
+  //       .clk(clk_sys),
+  //       .rst_core_n(rst_core_n) // rst for Ibex only
+  // );
 
   // Wishbone wrapped Ibex Core
   wb_ibex_top #( 
@@ -140,8 +141,8 @@ module ibex_simple_system #(
    u_wb_ibex_top
      (.clk                  (clk_sys),
       .rst_n                (rst_core_n),
-      .instr_wb             (wbm[2]),
-      .data_wb              (wbm[1]),
+      .instr_wb             (wbm[1]),
+      .data_wb              (wbm[0]),
 
       .test_en              (1'b0),
       .ram_cfg              ('0),
@@ -150,11 +151,13 @@ module ibex_simple_system #(
       .boot_addr            (32'h80000000),
 
       .irq_software         (1'b0),
-      .irq_timer            (pit_irq),  // Connect PIT's Interrupt Request signal
-      .irq_external         (gpio_int),
+      .irq_timer            (1'b0),
+      // .irq_timer            (pit_irq),  // Connect PIT's Interrupt Request signal
+      // .irq_external         (gpio_int),
+      .irq_external         (1'b0),
       `ifdef UART_NO_FIFO
-      .irq_fast             ({9'b0, uart_tx_int_o, uart_rx_int_o, 2'b0, i2c_int, 1'b0}),
-      // .irq_fast             ({9'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0}),
+      // .irq_fast             ({9'b0, uart_tx_int_o, uart_rx_int_o, 2'b0, i2c_int, 1'b0}),
+      .irq_fast             ({9'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0, 1'b0}),
       `else
       .irq_fast             ({9'b0, uart_tx_int_o, uart_rx_int_o, uart_txfifo_int_o, uart_rxfifo_int_o, i2c_int, 1'b0}),
       `endif
@@ -197,40 +200,41 @@ module ibex_simple_system #(
 //   );
 
   // GPIO module
-  wb_gpio u_gpio(
-    .wb(wbs[0]),
-
-    .int_o        (gpio_int),
-    .aux_i        (gpio_aux),         // Multiplexed outputs can be connected here
-    .ext_pad_i    (ext_pad_i),    
-    .ext_pad_o    (gpio_o),    
-    .ext_padoe_o  (gpio_oe)    
-  );
+  // wb_gpio u_gpio(
+  //   .wb(wbs[0]),
+  //
+  //   .int_o        (gpio_int),
+  //   .aux_i        (gpio_aux),         // Multiplexed outputs can be connected here
+  //   .ext_pad_i    (ext_pad_i),
+  //   .ext_pad_o    (gpio_o),
+  //   .ext_padoe_o  (gpio_oe)
+  // );
   // Connect UART tx to gpio aux
-  assign gpio_aux = {{(`GPIO_IOS-2){1'b0}}, uart_tx_o, 1'b0};
+  // assign gpio_aux = {{(`GPIO_IOS-2){1'b0}}, uart_tx_o, 1'b0};
+  // assign gpio_aux = {{(`GPIO_IOS-2){1'b0}}, 1'b0, 1'b0};
 
   // UART module
-  wb_uart u_uart(
-    .wb(wbs[2]),
-    
-    
-    // .i_cts_n		(uart_ncts_i),
-    `ifdef UART_NO_FIFO
-    `else
-    .i_cts_n    (1'b0),
-    .o_rts_n		(uart_nrts_o),
-    .o_uart_rxfifo_int	(uart_rxfifo_int_o),
-    .o_uart_txfifo_int	(uart_txfifo_int_o),
-    `endif
-    
-    .o_uart_rx_int	(uart_rx_int_o),
-    .o_uart_tx_int	(uart_tx_int_o),  
-
-    // .i_uart_rx		(uart_rx_i),
-    // .o_uart_tx		(uart_tx_o)
-    .i_uart_rx    (ext_pad_i[0]),
-    .o_uart_tx    (uart_tx_o)
-  );
+//   wb_uart u_uart(
+//     .wb(wbs[2]),
+//
+//
+//     // .i_cts_n		(uart_ncts_i),
+//     `ifdef UART_NO_FIFO
+//     `else
+//     .i_cts_n    (1'b0),
+//     .o_rts_n		(uart_nrts_o),
+//     .o_uart_rxfifo_int	(uart_rxfifo_int_o),
+//     .o_uart_txfifo_int	(uart_txfifo_int_o),
+//     `endif
+//
+//     .o_uart_rx_int	(uart_rx_int_o),
+//     .o_uart_tx_int	(uart_tx_int_o),
+//
+//     // .i_uart_rx		(uart_rx_i),
+//     // .o_uart_tx		(uart_tx_o)
+//     .i_uart_rx    (ext_pad_i[0]),
+//     .o_uart_tx    (uart_tx_o)
+//   );
 
 
   // I2C module debouncer
@@ -265,14 +269,14 @@ module ibex_simple_system #(
   // );
 
   // Programmable Interrupt Timer module
-  wb_pit u_pit (
-      .wb(wbs[1]),
-
-      .pit_irq_o  (pit_irq)
-  );
+  // wb_pit u_pit (
+  //     .wb(wbs[1]),
+  //
+  //     .pit_irq_o  (pit_irq)
+  // );
 
   wb_spi_flash u_spi_flash (
-      .wb(wbs[3]),
+      .wb(wbs[0]),
       .o_qspi_sck     (o_qspi_sck),
       .o_qspi_cs_n    (o_qspi_cs_n),
       .o_qspi_mod     (o_qspi_mod),
@@ -287,8 +291,8 @@ module ibex_simple_system #(
        wb_interconnect_sharedbus
          #(.numm      (NUM_MASTERS),
            .nums      (NUM_SLAVES),
-           .base_addr ('{gpio_base_addr, pit_base_addr, uart_base_addr, spi_flash_base_addr}),
-           .size      ('{gpio_size, pit_size, uart_size, spi_flash_size}))
+           .base_addr ('{spi_flash_base_addr}),
+           .size      ('{spi_flash_size}))
        u_wb_interconnect
          (.wbm, .wbs);
 endmodule
